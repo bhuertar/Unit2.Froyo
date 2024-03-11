ee73afbdec31172601a2134f4c993f43190d6e70
BryanHuerta <bhuertar@DESKTOP-OUBEMJ8>
2024-03-11 14:28:39 -0500

    created the function that goes through strings

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+// Create a function called countFroyoFlavors
 const countFroyoFlavors = () => {
-  const userInput = prompt(`Enter a list of comma-separated froyo flavors: `);
-  // console.log(userInput);
+  const userInput = prompt(`Enter a list of comma-separated froyo flavors (Case Sensative): `);
 
+  //Split input string into an array of flavors
   const flavors = userInput.split(`,`);
+
+  // Create an object to store the count of each flavor
+  let flavorCount = {};
+
+  // Go through flavors and update count
+  for( const flavor of flavors) {
+    if(flavorCount[flavor]) {
+      flavorCount[flavor]++;
+    } else {
+      flavorCount[flavor] = 1;
+    }
+  }
+  // return total amount of scoops associated with the flavor
+  console.table(flavorCount);
 }
 
 countFroyoFlavors();
+
+
+
+// return total amount of scoops associated with the flavor
