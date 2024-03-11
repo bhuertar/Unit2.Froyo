16559ab78d9f638e3c0efa05fb201b6cd5b3c708
BryanHuerta <bhuertar@DESKTOP-OUBEMJ8>
2024-03-11 14:07:11 -0500

    added prompt message

---
+++ index.js
@@ -0,0 +1,8 @@
+const countFroyoFlavors = () => {
+  const userInput = prompt(`Enter a list of comma-separated froyo flavors: `);
+  // console.log(userInput);
+
+  const flavors = userInput.split(`,`);
+}
+
+countFroyoFlavors();
